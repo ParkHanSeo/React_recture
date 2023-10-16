e4fa2cd65890caabd9d9c0ea225e0838eeccb431
parkjeehoon <gkstj8300@naver.com>
2023-10-16 23:02:54 +0900

    2023.10.16 함수표현식 & 화살표 함수

---
+++ javascaript_apply/function_expression.js
@@ -0,0 +1,14 @@
+// 함수표현식 & 화살표 함수
+let hello2 = () => "안녕하세요 여러분";
+console.log(helloB());
+console.log(hello2);
+
+let hello = function(){
+      return "안녕하세요 여러분";
+}
+
+
+
+function helloB(){
+      return "안녕하세요 여러분";
+}
